15f1df693ba6233a4743b50fb9dbe0f3e7e0729e
CY68GI <caohongyu386@gmail.com>
2025-07-29 22:33:17 +0800

    格式化代码，使其简洁明了

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,17 +19,12 @@ const count = ref(0)
 
   <p>
     查看
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >，官方的 Vue + Vite 启动器
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>，官方的 Vue + Vite 启动器
   </p>
   <p>
     在
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue 文档扩展指南</a
-    >中了解更多关于 Vue IDE 支持的信息。
+    <a href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support" target="_blank">Vue 文档扩展指南</a>中了解更多关于 Vue IDE
+    支持的信息。
   </p>
   <p class="read-the-docs">点击 Vite 和 Vue 标志了解更多信息</p>
 </template>
